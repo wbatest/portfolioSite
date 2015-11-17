6a61456824c0ec0afd9cac555617ba85ab45c161
Bates Tillman <w.bates.tillman@gmail.com>
2015-11-17 16:16:28 -0600

    Issues with the Job Descriptions

--- inc/resume.php
+++ inc/resume.php
@@ -1,26 +1,82 @@
 <div class="contentWrap">	
 	<div class="row resume">
 		<section class="jobs">
-			<article><img src="img/rmyc.png"></article>
-			<div class="jobDesc clearfix">
-				<p>
-					I lived out of a tent in campgrounds throughout northwest Colorado, based out of Steamboat Springs. As a part of a 10 person crew I was cutting down standing dead trees killed by the mountain pine beetle and cutting them in to firewood lengths.
-				</p>
-				<p>
-					During this time period I was able to get skilled with a chainsaw in a short period of time. It was a major mental victory to go from being slightly terrified of this implement of destruction to being totally comfortable with it in my hands. During this time I reveled in working with people that lived a very different life than me up to that point, and I learned a great deal about how to interact with people I don’t agree with, especially when they are in the position of power.
-				</p>
-			</div>
-			<article><img src="img/ski-patrol.png"></article>
-			<div class="jobDesc clearfix"></div>
-			<article><img src="img/canyons.png"></article>
-			<div class="jobDesc clearfix"></div>
+			<article data-job="rocky"><img src="img/rmyc.png"></article>
+			<div class="jobDesc clearfix" data-job="rocky">
 
-			<article><a href="#"><img src="img/fire.png"></a></article>
-			<div class="jobDesc clearfix"></div>
-			<article><a href="#"><img src="img/painting.png"></a></article>
-			<div class="jobDesc clearfix"></div>
-			<article><a href="#"><img src="img/sava-tree.png"></a></article>
-			<div class="jobDesc clearfix"></div>
+				<div class="employ">
+					<h3>Rocky Mountain Youth Corps</h3>
+					<h5>Steamboat Springs, CO Summer / Fall 2009</h5>
+				</div>
+				<div class="employ">
+					<p>
+						During this time period I was able to get skilled with a chainsaw in a short period of time. It was a major mental victory to go from being slightly terrified of this implement of destruction to being totally comfortable with it in my hands. During this time I reveled in working with people that lived a very different life than me up to that point, and I learned a great deal about how to interact with people I don’t agree with, especially when they are in the position of power.
+					</p>
+				</div>
+			</div>
+			<article data-job="canyons"><img src="img/ski-patrol.png"></article>
+			<div class="jobDesc clearfix" data-job="canyons">
+				<div class="employ">
+					<h3 >The Canyons Ski Resort</h3>
+					<h5 >Park City, UT Winter 2009 10 / 2010 11</h5>
+					<h3>Mount Hood Meadows Ski Resort</h3>
+					<h5>Mount Hoood, OR Winter 2011 12 / 2012 13</h5>
+				</div>
+				<div class="employ">
+					<p>
+						During this time period I was able to get skilled with a chainsaw in a short period of time. It was a major mental victory to go from being slightly terrified of this implement of destruction to being totally comfortable with it in my hands. During this time I reveled in working with people that lived a very different life than me up to that point, and I learned a great deal about how to interact with people I don’t agree with, especially when they are in the position of power.
+					</p>
+				</div>
+			</div>
+			<article data-job="canyons2"><img src="img/canyons.png"></article>
+			<div class="jobDesc clearfix" data-job="canyons2">
+				<div class="employ">
+					<h3>The Canyons Ski Resort</h3>
+					<h5>Park City, UT Summer / Fall 2010</h5>
+				</div>
+				<div class="employ">
+					<p>
+						During this time period I was able to get skilled with a chainsaw in a short period of time. It was a major mental victory to go from being slightly terrified of this implement of destruction to being totally comfortable with it in my hands. During this time I reveled in working with people that lived a very different life than me up to that point, and I learned a great deal about how to interact with people I don’t agree with, especially when they are in the position of power.
+					</p>
+				</div>
+			</div>
+
+			<article data-job="miller"><img src="img/fire.png"></article>
+			<div class="jobDesc clearfix" data-job="miller">
+				<div class="employ">
+					<h3>Miller Timber Services</h3>
+					<h5>Philomath, OR Summer / Fall 2011</h5>
+				</div>
+				<div class="employ">
+					<p>
+						During this time period I was able to get skilled with a chainsaw in a short period of time. It was a major mental victory to go from being slightly terrified of this implement of destruction to being totally comfortable with it in my hands. During this time I reveled in working with people that lived a very different life than me up to that point, and I learned a great deal about how to interact with people I don’t agree with, especially when they are in the position of power.
+					</p>
+				</div>
+			</div>
+			<article data-job="fitz"><img src="img/painting.png"></article>
+			<div class="jobDesc clearfix" data-job="fitz">
+				<div class="employ">
+					<h3>Fitzpatrick Painting</h3>
+					<h5>Albany, OR Summer / Fall 2012</h5>
+				</div>
+				<div class="employ">
+					<p>
+						During this time period I was able to get skilled with a chainsaw in a short period of time. It was a major mental victory to go from being slightly terrified of this implement of destruction to being totally comfortable with it in my hands. During this time I reveled in working with people that lived a very different life than me up to that point, and I learned a great deal about how to interact with people I don’t agree with, especially when they are in the position of power.
+					</p>
+				</div>
+			</div>
+			<article data-job="croix"><img src="img/sava-tree.png"></article>
+			<div class="jobDesc clearfix" data-job="croix">
+				<div class="employ">
+					<h3>St. Croix SavaTree</h3>
+					<h5>Hopkins, MN 2013</h5>
+				</div>
+				<div class="employ">
+					<p>
+						Sometimes the lesson learned is a bitter pill to swallow. This was a rewarding job that reaffirmed my ability to work at a high skill level in stressful situations. But unfortunately this great experience was prematurely ended with a bitter lesson in how not to treat people. These lessons are best learned from up close, but better to be observed from a distance.
+					</p>
+				</div>
+			</div>
 		</section>
 	</div>
 </div>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
+var windowState;
+
 $(document).ready(function() {
 	console.log("This is the file main.js speaking.");
 
 	$("body").on('click','#navToggle',function() {
 		navToggle();
 	});
+
+	$("body").on('click', '.jobs > article', function() {
+		jobToggle($(this).data('job'));
+	});
+
+	getWindowState();
+	jobSort(windowState);
 
 });
 
@@ -15,10 +24,79 @@ $(window).resize(function() {
 	} else {
 		$("#topNav").css("display","none");
 	}
+
+	var currentWindowState = windowState;
+	getWindowState();
+	if (currentWindowState != windowState) {
+		console.log("CHANGE!");
+		jobSort(windowState);
+	}
+
 });
+
+function getWindowState() {
+	if ($(window).width() <= 639) {
+		windowState = "mobile";
+	} else if ($(window).width() <= 959) {
+		windowState = "tablet";
+	} else {
+		windowState = "desktop";
+	}
+	console.log(windowState);
+}
 
 function navToggle() {
 	console.log("This is the function navToggle() speaking.");
-	$("#topNav").slideToggle();
+	$("#topNav").stop().slideToggle();
 }
+
+function jobToggle(jobToToggle) {
+	console.log("This is the function jobToggle() speaking.");
+	console.log(jobToToggle);
+	$('.jobDesc[data-job="' + jobToToggle + '"]').stop().slideToggle();
+}
+
+function jobSort(sortState) {
+	// if($(".buffer").length) {
+	// 	$.each($(".buffer")).remove();
+	// }
+	if (sortState == "mobile") {
+		$.each($(".jobDesc"), function() {
+			console.log($(this).data("job"));
+			$(this).insertAfter('article[data-job="' + $(this).data("job") + '"]');
+			$(this).before("<div class='buffer clearfix'></div>");
+		});
+	}
+	else if (sortState == "tablet") {
+
+		var columnCount = 2;
+		var descCount = 0;
+		$.each($(".jobDesc"), function() {
+			descCount++;
+			console.log($(this).data("job"));
+			$(this).insertAfter('article:nth-of-type('+ columnCount +')');
+			$(this).before("<div class='buffer clearfix'></div>");
+			if (descCount == columnCount) {
+				columnCount = columnCount + 2;
+			}
+		});
+
+	}
+	else {
+
+		var columnCount = 3;
+		var descCount = 0;
+		$.each($(".jobDesc"), function() {
+			descCount++;
+			console.log($(this).data("job"));
+			$(this).insertAfter('article:nth-of-type('+ columnCount +')');
+			$(this).before("<div class='buffer clearfix'></div>");
+			if (descCount == columnCount) {
+				columnCount = columnCount + 3;
+			}
+		});
+
+	}
+}
+
 
